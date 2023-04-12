9234df0fb17f34b6e8f468387c95d4dc3639ed35
PiqqiDesigns <eric.otuvaka@gmail.com>
2023-04-12 09:36:11 -0700

    feat: basic tagToIcon function complete

--- src/sections/Explore.tsx
+++ src/sections/Explore.tsx
@@ -31,7 +31,10 @@ export function Explore({ exploreRecipes }: IExplore) {
 	// TO DO: create a tag -> svg function that takes a tag and will
 	//	return a JSX element of its svg icon
 	// 	for each tag, call the function and use the JSX icon it returns
-	function getIconFromTag(tag: string) {
+	function getIconFromTag(tag: string | undefined) {
+		if (tag === undefined || tag === "") {
+			return <FontAwesomeIcon icon={faBowlFood} />;
+		}
 		const lowerTag = tag.toLowerCase();
 
 		interface ITagIconMap {
@@ -49,9 +52,10 @@ export function Explore({ exploreRecipes }: IExplore) {
 			"dessert": <FontAwesomeIcon icon={faIceCream} />,
 			"snack": <FontAwesomeIcon icon={faCubesStacked} />,
 			"spicy": <FontAwesomeIcon icon={faPepperHot} />,
+			"": <FontAwesomeIcon icon={faBowlFood} />,
 		};
 
-		return tagToIconMap[lowerTag] || <FontAwesomeIcon icon={faBowlFood} />;
+		return tagToIconMap[lowerTag];
 	}
 
 	return (
@@ -61,18 +65,18 @@ export function Explore({ exploreRecipes }: IExplore) {
 					exploreRecipes.value.map((recipe, i) => (
 						<div
 							key={i}
-							className={`flex h-64 flex-col justify-end border border-black bg-red-500 transition-all duration-300 hover:cursor-pointer hover:border-white`}
+							className={`flex h-64 flex-col justify-between border border-neutral-500 bg-neutral-900 transition-all duration-300 hover:cursor-pointer hover:border-white`}
 							onClick={() => {
 								setFocusedView(true);
 								setFocusedRecipe(recipe);
 							}}
 						>
-							<div className="flex items-center justify-center bg-blue-500 text-center">
-								{recipe.tags[0]}
-								{", "}
-								{recipe.tags[1]}
-								{", "}
-								{recipe.tags[2]}
+							<div className="mt-12 flex w-full items-center justify-center text-center">
+								<div className="flex w-4/5 items-center justify-center gap-2 rounded-md bg-black py-8 text-2xl">
+									{getIconFromTag(recipe.tags[0])}
+									{getIconFromTag(recipe.tags[1])}
+									{getIconFromTag(recipe.tags[2])}
+								</div>
 							</div>
 							<div className="bg-gradient-to-t from-neutral-800 to-transparent py-4 px-4 pt-2">
 								<h3 className="mb-2 text-xl font-bold">{recipe.name}</h3>
